eeca742073ca3197bc5f98fcc6c496194c4019f2
ruhit3 <ruhit1011@gmail.com>
2024-04-29 15:52:42 +0600

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,23 @@ import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
-        Socket clientSocket = null;
+        Socket clientSocket;
         try (ServerSocket serverSocket = new ServerSocket(4221)) {
             serverSocket.setReuseAddress(true);
             clientSocket = serverSocket.accept(); // Wait for connection from client.
-            System.out.println("accepted new connection");
+            System.out.println("Accepted new connection!");
+
+            StringBuilder data = new StringBuilder();
+            var inputStream = clientSocket.getInputStream();
+            while (inputStream.available() != 0) {
+                char c = (char) inputStream.read();
+                data.append(c);
+            }
+            System.out.println("Data: " + data);
+
+            String response = "HTTP/1.1 200 OK\r\n\r\n";
+            clientSocket.getOutputStream().write(response.getBytes());
+
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
