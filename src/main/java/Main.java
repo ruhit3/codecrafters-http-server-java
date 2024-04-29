b57f371ea8afcda1ff3ba322c5750868c4b013a0
ruhit3 <ruhit1011@gmail.com>
2024-04-29 16:06:59 +0600

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,10 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
+
+    static final String OK_RESPONSE = "HTTP/1.1 200 OK\r\n\r\n";
+    static final String NOT_FOUND_RESPONSE = "HTTP/1.1 404 Not Found\r\n\r\n";
+
     public static void main(String[] args) {
         Socket clientSocket;
         try (ServerSocket serverSocket = new ServerSocket(4221)) {
@@ -18,8 +22,17 @@ public class Main {
             }
             System.out.println("Data: " + data);
 
-            String response = "HTTP/1.1 200 OK\r\n\r\n";
-            clientSocket.getOutputStream().write(response.getBytes());
+            var dataStrings = new String(data).split("\\r\\n");
+            var requestInfo = dataStrings[0];
+            var requestInfoStrings = requestInfo.split(" ");
+
+
+            if (requestInfoStrings[1].equals("/")) {
+                clientSocket.getOutputStream().write(OK_RESPONSE.getBytes());
+            } else {
+                clientSocket.getOutputStream().write(NOT_FOUND_RESPONSE.getBytes());
+            }
+
 
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
